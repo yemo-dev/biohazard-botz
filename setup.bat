6c4b285280a7d45c375a0787360826f45e0f50c9
yemobyte <yemobyte@gmail.com>
2026-02-23 07:35:15 -0800

    feat: add cross-platform setup scripts (setup.bat, setup.sh)

---
+++ setup.bat
@@ -0,0 +1,12 @@
+@echo off
+title Biohazard Botz Setup
+echo B I O H A Z A R D - B O T Z
+echo -----------------------------------
+echo [1] Installing dependencies...
+call npm install
+echo -----------------------------------
+echo [2] Dependencies installed.
+echo Starting the bot...
+echo -----------------------------------
+npm start
+pause
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "B I O H A Z A R D - B O T Z"
+echo "-----------------------------------"
+echo "[1] Installing dependencies..."
+npm install
+echo "-----------------------------------"
+echo "[2] Dependencies installed."
+echo "Starting the bot..."
+echo "-----------------------------------"
+npm start
